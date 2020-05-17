41a2a861b0998d87804e67c98b7fbf41148046c0
Melvin <melvin@ylly.fr>
2020-05-17 13:32:07 +0200

    ref #12 :
    * Add Edit api

--- src/Controller/BudgetCardController.php
+++ src/Controller/BudgetCardController.php
@@ -61,19 +61,54 @@ class BudgetCardController extends AbstractController
     }
 
     /**
+     * @Route("/api/budget-card/{id}", name="api_budget_card_edit", methods={"PATCH"})
+     */
+    public function editBudgetCard(int $id, Request $request, EntityManagerInterface $emi)
+    {
+        $req = json_decode($request->getContent(), true);
+
+        $title = $req['title'];
+        $ceil = $req['ceil'];
+        $limitDate = $req['limitDate'];
+        $currentMoney = $req['currentMoney'];
+        $userId = $req['userId'];
+
+        try {
+            $budgetCardToEdit = $this->getDoctrine()->getRepository(BudgetCard::class)->findBy(["id" => $id]);
+            $budgetCardToEdit = $budgetCardToEdit[0];
+
+            $user = $this->getDoctrine()->getRepository(User::class)->findBy(["id" => $userId]);
+            $budgetCardToEdit->setTitle($title);
+            $budgetCardToEdit->setCeil($ceil);
+            $budgetCardToEdit->setLimitDate(new DateTime($limitDate));
+            $budgetCardToEdit->setCurrentMoney($currentMoney);
+            $budgetCardToEdit->setUser($user[0]);
+            $budgetCardToEdit->setCreatedAt(new DateTime());
+
+            $emi->persist($budgetCardToEdit);
+            $emi->flush();
+
+            return $this->json($budgetCardToEdit, 201, [], ["groups" => "budget-card-create"]);
+        } catch (NotEncodableValueException $e) {
+            return $this->json([
+                'status' => 400,
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    /**
      * @Route("/api/budget-card/{id}", name="api_budget_card_delete", methods={"DELETE"})
      */
     public function deleteBudgetCard(int $id, EntityManagerInterface $emi)
     {
-      
-
         try {
             $budgetCard = $this->getDoctrine()->getRepository(BudgetCard::class)->findBy(["id" => $id]);
 
             $emi->remove($budgetCard[0]);
             $emi->flush();
 
-            return $this->json($budgetCard[0], 204);
+            return $this->json($budgetCard[0], 204, [], ["groups" => "budget-card-create"] );
         } catch (NotEncodableValueException $e) {
             return $this->json([
                 'status' => 400,
